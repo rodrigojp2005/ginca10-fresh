6c947e0e850a7c3b8e4a1ec312c0184588eced69
rodrigojp2005 <rodrigojp2005@gmail.com>
2025-08-01 13:47:25 -0300

    remoçao da view dashboard e ajustes de rotas .

--- routes/web.php
+++ routes/web.php
@@ -42,10 +42,6 @@ Route::get('/', function (Request $request) {
     return view('welcome', compact('locations'));
 })->name('home');
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
-
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
