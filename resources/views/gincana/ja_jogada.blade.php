af36ba90ddbf913bdf69fd89540125d942a66849
rodrigojp2005 <rodrigojp2005@gmail.com>
2025-08-12 20:09:17 -0300

    removida a duplicata

--- resources/views/gincana/ja_jogada.blade.php
+++ resources/views/gincana/ja_jogada.blade.php
@@ -1,6 +1,5 @@
 @extends('layouts.app')
 @section('content')
-<script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
 <script>
     document.addEventListener('DOMContentLoaded', function() {
         Swal.fire({
